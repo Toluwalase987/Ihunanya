6ddfb00b374c8904fe900d7ffefe7ae0305e2de2
Toluwalase987 <toluwalase987@gmail.com>
2024-02-13 16:01:58 +0100

    Marquee fixed

---
+++ src/assets/data.jsx
@@ -0,0 +1,45 @@
+import nanya1 from "../assets/images/nanya1.jpeg";
+import nanya4 from "../assets/images/nanya4.jpeg";
+import nanya3 from "../assets/images/nanya3.jpeg";
+import nanya5 from "../assets/images/nanya5.jpeg";
+import nanya6 from "../assets/images/nanya6.jpeg";
+import nanya7 from "../assets/images/nanya7.jpeg";
+import nanya8 from "../assets/images/nanya8.jpeg";
+import nanya9 from "../assets/images/nanya9.jpeg";
+import nanya10 from "../assets/images/nanya10.jpeg";
+import nanya11 from "../assets/images/nanya11.jpeg";
+import nanya12 from "../assets/images/nanya12.jpeg";
+import chava1 from "../assets/images/Chava.jpeg";
+import chava2 from "../assets/images/Chava1.jpeg";
+import chava0 from "../assets/images/Chava2.jpeg";
+import chava3 from "../assets/images/Chava4.jpeg";
+import calls from "../assets/images/calls.jpeg";
+import calls1 from "../assets/images/calls1.jpeg";
+import nanyavid from "../assets/images/nanyavid.mp4";
+import iMessage from "../assets/images/iMessage.jpeg";
+
+export const chava = [
+    {image: nanya8},
+    {image: chava0},
+    {image: chava1},
+    {image: chava2},
+    {image: chava3},
+]
+
+export const pretty = [
+    {image: nanya6},
+    {image: nanya9},
+    {image: nanya3},
+    {image: nanya4},
+]
+
+export const catwoman = [
+    {image: nanya12},
+    {image: nanya10},
+    {image: nanya11},
+]
+
+export const call = [
+    {image: calls},
+    {image: calls1},
+]
